e377ec26f258e426aa8d27109eb5364df189ae2f
sarika-cloud <sarikakan6@gmail.com>
2025-06-20 04:03:32 -0400

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -8,13 +8,7 @@ DOMAIN_NAME="daws84s.life"
 
 for instance in ${INSTANCE_TYPE[@]}
 do
-   Instance_ID=$(aws ec2 run-instances / 
-     --image-id $AMI_ID /
-     --instance-type t2.micro /
-     --security-group-ids $SG_ID /
-     --tag-specifications  "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" /
-     --query "Instances[0].InstanceId" /
-     --output text)
+   Instance_ID=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type t2.micro  --security-group-ids $SG_ID     --tag-specifications  "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"  --query "Instances[0].InstanceId"     --output text)
   if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
