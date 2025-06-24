aab2d908438a3f7ea824ca43b2fca058095eea66
sarika-cloud <sarikakan6@gmail.com>
2025-06-24 01:44:46 -0400

    hello world started

--- mongodb.sh
+++ mongodb.sh
@@ -5,40 +5,40 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+B="\e[34m"
 N="\e[0m"
 
 log_folder="/var/log/roboshop-logs"
-script_name=$(echo $0 | cut -d "." -f1)
+script_name=$(basename "$0")
 log_file="$log_folder/$script_name.log"
+LOG_FILE=$log_file  # Fixes empty LOG_FILE issue
 
-mkdir -p $LOGS_FOLDER
-echo "Scipt started executing at: $(date)" | tee -a $LOG_FILE
+mkdir -p $log_folder
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-#check the user has root privileges or not
-if [ $USERID -eq 0 ]
-
-then 
-   echo -e "$G User has root access " | tee -a $LOG_FILE
+# Check if the user has root privileges
+if [ $USERID -eq 0 ]; then
+  echo -e "${G}User has root access${N}" | tee -a $LOG_FILE
 else
-   echo -e "$R User doesn't have root access " | tee -a $LOG_FILE
-   exit 1 #give other than 0 upto 127
+  echo -e "${R}User doesn't have root access${N}" | tee -a $LOG_FILE
+  exit 1
 fi
 
-# validate functions takes input as exit status,what command they tried to install
+# Validate function
 VALID() {
-if [ $1 -eq 0 ]
-then
-    echo -e " $N Installation of $2 is ....$G Successful"
-else
-    echo -e "$N Installation of $2 is ....$R Failure"
+  if [ $1 -eq 0 ]; then
+    echo -e "$G Installation of $2 is ....$G Successful$N"
+  else
+    echo -e "$R Installation of $2 is ....$R Failure$N"
     exit 1
-fi 
+  fi
 }
 
+# Start MongoDB setup
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
-VALID $? "copying Mongodb repo"
+VALID $? "copying MongoDB repo"
 
-dnf install mongodb-org -y &>>$LOG_FILE=/tmp/mongodb.log
+dnf install mongodb-org -y &>>$LOG_FILE
 VALID $? "Installing mongodb server"
 
 systemctl enable mongod &>>$LOG_FILE
@@ -47,7 +47,7 @@ VALID $? "Enabling MongoDB"
 systemctl start mongod &>>$LOG_FILE
 VALID $? "Starting MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 VALID $? "Editing MongoDB conf file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
