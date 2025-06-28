dab27f64c58931195937dc352ddf5128d43f6788
sarika-cloud <sarikakan6@gmail.com>
2025-06-28 01:08:30 -0400

    hello world started

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-01bc7ebe005fb1cb2" # replace with your SG ID
+SG_ID="sg-06ac706b2ca290189" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
-DOMAIN_NAME="daws84s.site" # replace with your domain
+ZONE_ID="Z05650453EAGV8BJNVHGB" # replace with your ZONE ID
+DOMAIN_NAME="daws84s.life" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
 for instance in $@
